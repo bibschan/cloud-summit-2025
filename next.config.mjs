b8048f7fa2c7a24f03764a9f681796fa9ebcf97d
bibianasouza <souzabibiana@hotmail.com>
2024-11-13 22:54:19 -0800

    removed index to test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
-  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
+  basePath: process.env.NODE_ENV === "production" ? "/out" : undefined,
   assetPrefix: process.env.NODE_ENV === "production" ? "/" : undefined,
 };
 export default nextConfig;
