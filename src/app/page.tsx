ca5c807c5deb5873f63d00a2c5773bfda5b1d0e4
bibianasouza <souzabibiana@hotmail.com>
2024-11-13 22:57:57 -0800

    re-added html file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -339,12 +339,12 @@ export default function Home() {
               <h3 className="text-lg font-semibold text-white mb-4">Archive</h3>
               <ul className="space-y-2">
                 <li>
-                  {/* <Link
+                  <Link
                     href="/2024.html"
                     className="hover:text-green-500 transition-colors"
                   >
                     Cloud Summit 2024
-                  </Link> */}
+                  </Link>
                 </li>
               </ul>
             </div>
