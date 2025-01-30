bb8ced597932c7e505b0c20a708f5149fa07b28d
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-01-30 09:45:37 -0800

    form added but need to clean up code and separate form to separate component.

--- src/app/justification-letter/page.tsx
+++ src/app/justification-letter/page.tsx
@@ -2,7 +2,8 @@ import JustificationLetterPDF from "@/components/justification-letter-pdf";
 
 export default function JustificationLetter() {
   return (
-    <main>
+    <main className="bg-gray-900 text-white pb-6">
+      <h1 className="bg-gray-900 p-4 sm:p-8 text-2xl font-bold">Conference Attendance Justification Letter</h1>
       <JustificationLetterPDF />
     </main>
   )
--- src/components/justification-letter-pdf.tsx
+++ src/components/justification-letter-pdf.tsx
@@ -12,7 +12,7 @@ export default function JustificationLetterPDF() {
             specificTeam: '',
             travelCost: '',
             agenda: '',
-            goals: '',
+            goal: '',
             name: '',
             title: '',
         }
@@ -30,7 +30,7 @@ export default function JustificationLetterPDF() {
         return () => window.removeEventListener("resize", handleResize)
     }, [])
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
         setFormData(prevData => ({
             ...prevData,
@@ -83,51 +83,111 @@ export default function JustificationLetterPDF() {
     }
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white p-4 sm:p-8">
+        <div className="min-h-screen bg-gray-900 text-white md:flex md:gap-2">
 
-                <h1 className="text-2xl font-bold">Conference Attendance Justification Letter</h1>
+            <form className="rounded-lg border text-card-foreground shadow-sm bg-white border-gray-700 max-w-lg md:max-w-md mx-auto mt-8 p-8">
+                <label className="block text-sm font-medium mb-1 text-black">Manager&apos;s Name</label>
+                <input
+                    name="managerName"
+                    value={formData.managerName}
+                    onChange={handleChange}
+                    placeholder="Enter your manager's name"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
 
-            <div ref={pdfRef} className="rounded-lg border text-card-foreground shadow-sm bg-gray-800 border-gray-700 max-w-4xl mx-auto mt-8 p-8">
+                <label className="block text-sm font-medium mb-1 text-black">Company</label>
+                <input
+                    name="company"
+                    value={formData.company}
+                    onChange={handleChange}
+                    placeholder="Enter your company name"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Specific Project</label>
+                <input
+                    name="specificProject"
+                    value={formData.specificProject}
+                    onChange={handleChange}
+                    placeholder="Enter your specific project"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Specific Team/Goal</label>
+                <input
+                    name="specificTeam"
+                    value={formData.specificTeam}
+                    onChange={handleChange}
+                    placeholder="Enter your specific team/goal"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Travel Cost</label>
+                <input
+                    name="travelCost"
+                    value={formData.travelCost}
+                    onChange={handleChange}
+                    placeholder="Enter your travel cost"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Agenda</label>
+                <input
+                    name="agenda"
+                    value={formData.agenda}
+                    onChange={handleChange}
+                    placeholder="Enter the conference agenda"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Goal</label>
+                <input
+                    name="goal"
+                    value={formData.goal}
+                    onChange={handleChange}
+                    placeholder="specific goal, e.g., modernize IT infrastructure, enhance cloud security, improve operational efficiency"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="text-sm font-medium mb-1 text-black">Your Name</label>
+                <input
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Enter your name"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+                <label className="block text-sm font-medium mb-1 text-black">Your Title</label>
+                <input
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    placeholder="Enter your title"
+                    className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+                />
+
+            </form >
+
+            <section ref={pdfRef} className="rounded-lg border text-card-foreground shadow-sm bg-gray-800 border-gray-700 max-w-xl mx-auto mt-8 p-8 flex-grow">
                 <div className={`pdf-content ${isSmallScreen ? "text-sm" : "text-base"}`}>
                     <h3 className="mb-4"><span className="font-bold">Subject:</span> Request for Approval to Attend Cloud Summit 2025 </h3>
-                    <p className="whitespace-pre-wrap">{formData.title}</p>
                 </div>
                 <div className="prose max-w-none">
 
-                    <p className="mb-4">Dear
-                    <input
-                            name="managerName"
-                            value={formData.managerName}
-                            onChange={handleChange}
-                            placeholder="Enter your manager's name"
-                            className="w-56 bg-gray-800 border-gray-700 rounded px-2 ml-2"
-                    />,
+                    <p className="mb-4">Dear {formData.managerName || "[Boss's Name]"} ,
                     </p>
                     <p className="mb-4">
-                        I am writing to request approval to attend <a href="https://www.cloudsummit.com" target="_blank" rel="noopener noreferrer">Cloud Summit 2025</a> scheduled for May 27th, 2025, in Vancouver. This event is Western Canada&#39;s leading cloud-focused conference, bringing together top industry experts, innovators, and practitioners to discuss emerging technologies, best practices, and trends in cloud computing.
+                        I am writing to request approval to attend <a href="https://www.cloudsummit.com" target="_blank" rel="noopener noreferrer">Cloud Summit 2025</a> scheduled for May 27th, 2025, in Vancouver. This event is Western Canada&apos;s leading cloud-focused conference, bringing together top industry experts, innovators, and practitioners to discuss emerging technologies, best practices, and trends in cloud computing.
                     </p>
 
-                    <h3 className="mb-4 font-bold">Key Benefits to
-                        <input
-                            name="company"
-                            value={formData.company}
-                            onChange={handleChange}
-                            placeholder="Enter your company name"
-                            className="w-56 bg-gray-800 border-gray-700 rounded px-2 ml-2"
-                        />:
+                    <h3 className="mb-4 font-bold">Key Benefits to {formData.company || "[Company Name]"}:
                     </h3>
 
                     <ol className="mb-4 list-decimal list-inside flex flex-col gap-3">
                         <li>
                             <span className="font-bold">Expert Insights on Cloud Innovation</span>
-                            <p className="mx-4">Attending this summit will provide cutting-edge knowledge on cloud strategies, solutions, and emerging trends that could directly benefit our ongoing
-                            <input
-                            name="specificProject"
-                            value={formData.specificProject}
-                            onChange={handleChange}
-                            placeholder="Enter specific project/initiative"
-                            className="w-60 bg-gray-800 border-gray-700 rounded px-2 mx-2"
-                            /> by ensuring alignment with the latest industry advancements.
+                            <p className="mx-4">Attending this summit will provide cutting-edge knowledge on cloud strategies, solutions, and emerging trends that could directly benefit our ongoing {formData.specificProject || "[specific project/initiative]"} by ensuring alignment with the latest industry advancements.
                             </p>
                         </li>
                         <li>
@@ -137,15 +197,7 @@ export default function JustificationLetterPDF() {
                         </li>
                         <li>
                             <span className="font-bold">Skills Development</span>
-                            <p className="mx-4">The summit agenda includes technical sessions and keynote presentations designed to deepen expertise in cloud technologies. These sessions can enhance my ability to contribute to
-                            <input
-                            name="specificTeam"
-                            value={formData.specificTeam}
-                            onChange={handleChange}
-                            placeholder="Enter specific team/project goal"
-                            className="w-64 bg-gray-800 border-gray-700 rounded pl-2 mx-2"
-                            />
-                            and mentor colleagues on these topics.</p>
+                            <p className="mx-4">The summit agenda includes technical sessions and keynote presentations designed to deepen expertise in cloud technologies. These sessions can enhance my ability to contribute to {formData.specificTeam || "[specific team/project goal]"} and mentor colleagues on these topics.</p>
                         </li>
                         <li>
                             <span className="font-bold">Immediate ROI</span>
@@ -158,17 +210,17 @@ export default function JustificationLetterPDF() {
                     </h3>
                     <ul className="list-disc list-inside mb-4">
                         <li>Registration Fee: {currentDate > new Date('2025-03-01') ? '$60.00' : '$30.00'}</li>
-                        <li>Travel/Accommodation: [Cost, if applicable]</li>
+                        <li>Travel/Accommodation: {formData.travelCost || "[Cost, if applicable]"} </li>
                         <li>Total Estimated Investment: [Cost]</li>
                     </ul>
 
                     <h3 className="mt-4 font-bold">Alignment with Company Goals: </h3>
                     <p className="mb-4">
-                        The topics covered at the Cloud Summit, such as [specific agenda items or themes relevant to your role/team], directly align with our strategic objectives to [specific goal, e.g., modernize IT infrastructure, enhance cloud security, improve operational efficiency].
+                        The topics covered at the Cloud Summit, such as {formData.agenda || '[specific agenda items or themes relevant to your role/team]'}, directly align with our strategic objectives to {formData.goal || '[specific goal, e.g., modernize IT infrastructure, enhance cloud security, improve operational efficiency]'}.
                     </p>
 
                     <p className="mb-4">
-                        Thank you for considering this request. Attending the Cloud Summit 2025 will not only benefit my professional development but also directly support {formData.company || '[Company Name]'}&#39;s growth and innovation initiatives.
+                        Thank you for considering this request. Attending the Cloud Summit 2025 will not only benefit my professional development but also directly support {formData.company || '[Company Name]'}&apos;s growth and innovation initiatives.
                     </p>
                     <p className="mb-4">
                         Please let me know if you need any additional details or if we can discuss further.
@@ -176,26 +228,12 @@ export default function JustificationLetterPDF() {
 
                     <p className="mb-4">
                         Best regards,<br />
-                        <input
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            placeholder="Enter your name"
-                            className="w-full bg-gray-800 border-gray-700 rounded px-2 my-2 "
-                        />
-                        <br />
-                        <input
-                            name="title"
-                            value={formData.title}
-                            onChange={handleChange}
-                            placeholder="Enter your title"
-                            className="w-full bg-gray-800 border-gray-700 rounded px-2 my-2"
-                        />
-                        <br />
+                        {formData.name || '[Your Name]'}<br />
+                        {formData.title || '[Your Title]'}<br />
+                        {formData.company || '[Company Name]'}
                     </p>
                 </div>
-            </div>
-            <div className=" flex justify-end my-4 max-w-4xl mx-auto">
+                <div className=" flex justify-end my-4 max-w-4xl mx-auto">
                 <a onClick={handleSave} className="bg-blue-600 mx-2 px-4 py-2 rounded-md">
                     <img alt='Save as PDF' src='/justification-letter/download.svg'></img>
                 </a>
@@ -203,6 +241,8 @@ export default function JustificationLetterPDF() {
                     <img alt='Print' src='/justification-letter/printer.svg'></img>
                 </a>
             </div>
-        </div>
+            </section>
+
+        </div >
     )
 }
