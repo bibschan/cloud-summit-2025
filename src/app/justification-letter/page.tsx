20b45803be4a79d3863965a4c25047c9c85f15a1
bibianasouza <souzabibiana@hotmail.com>
2025-02-10 19:48:48 -0800

    cleaned up some things, ready to go

--- src/app/justification-letter/page.tsx
+++ src/app/justification-letter/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
+
 import JustificationLetterPDF from "@/components/justification-letter-pdf";
 import JustificationLetterForm from "@/components/justification-letter-form";
 import React, { useState, useEffect, useRef } from "react";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
-
 
 export interface FormDataType {
   managerName: string;
@@ -19,30 +19,38 @@ export interface FormDataType {
 }
 
 export default function JustificationLetter() {
-  const [formData, setFormData] = useState<FormDataType>(
-    {
-      managerName: '',
-      company: '',
-      specificProject: '',
-      specificTeam: '',
-      travelCost: 0,
-      agenda: '',
-      goal: '',
-      name: '',
-      title: '',
-    }
-  )
-
+  const [formData, setFormData] = useState<FormDataType>({
+    managerName: "",
+    company: "",
+    specificProject: "",
+    specificTeam: "",
+    travelCost: 0,
+    agenda: "",
+    goal: "",
+    name: "",
+    title: "",
+  });
 
   return (
     <main className=" py-20 bg-gray-900 text-white pb-6 w-full">
       <Nav />
-      <h1 className="bg-gray-900 mb-4 px-4 sm:px-8 text-4xl md:text-5xl md:mb-8 text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">Conference Attendance Justification Letter</h1>
-      <p className="text-center text-gray-400 text-sm px-4 mb-4 md:text-lg md:mb-8 md:max-w-4xl mx-auto">A justification letter is a formal written request for approval to attend a professional conference. The template below allows you to easily fill out the form and save/print it to send to your manager. Just fill out the fields below</p>
+      <h1 className="bg-gray-900 mb-4 px-4 sm:px-8 text-4xl md:text-5xl md:mb-8 text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+        Conference Attendance Justification Letter
+      </h1>
+      <p className="text-center text-gray-400 text-sm px-4 mb-4 md:text-lg md:mb-8 md:max-w-4xl mx-auto">
+        A justification letter is a formal written request for approval to
+        attend a professional conference. The template below allows you to
+        easily fill out the form and save/print it to send to your manager. Just
+        fill out the fields below
+      </p>
       <div className="flex flex-col md:flex-row gap-4 md:mx-2 md:w-auto justify-center px-4">
-        <JustificationLetterForm  formData={formData} setFormData={setFormData} />
+        <JustificationLetterForm
+          formData={formData}
+          setFormData={setFormData}
+        />
         <JustificationLetterPDF formData={formData} setFormData={setFormData} />
       </div>
+      <Footer />
     </main>
-  )
+  );
 }
--- src/components/justification-letter-form.tsx
+++ src/components/justification-letter-form.tsx
@@ -1,116 +1,127 @@
-import React from 'react'
-import { FormDataType } from '@/app/justification-letter/page';
+import React from "react";
+import { FormDataType } from "@/app/justification-letter/page";
 interface JustificationLetterFormProps {
-    formData: FormDataType;
-    setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
+  formData: FormDataType;
+  setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
 }
 
-export default function JustificationLetterForm({ formData, setFormData }: JustificationLetterFormProps) {
+export default function JustificationLetterForm({
+  formData,
+  setFormData,
+}: JustificationLetterFormProps) {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "travelCost") {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: parseFloat(value) || 0,
+      }));
+      return;
+    }
+    setFormData((prevFormData: FormDataType) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        if (name === 'travelCost') {
-            setFormData(prev => ({
-                ...prev,
-                [name]: parseFloat(value) || 0
-            }));
-            return;
-        }
-        setFormData((prevFormData: FormDataType) => ({
-            ...prevFormData,
-            [name]: value,
-        }));
-    };
+  return (
+    <form className="w-full rounded-lg border text-card-foreground shadow-sm bg-white border-gray-700 max-w-lg md:max-w-md mx-auto md:mx-0 p-8 max-h-fit">
+      <label className="text-sm font-medium mb-1 text-black">Your Name</label>
+      <input
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+        placeholder="Enter your name"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-    return (
-        <form className="w-full rounded-lg border text-card-foreground shadow-sm bg-white border-gray-700 max-w-lg md:max-w-md mx-auto md:mx-0 p-8 max-h-fit">
-            <label className="text-sm font-medium mb-1 text-black">Your Name</label>
-            <input
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                placeholder="Enter your name"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
+      <label className="block text-sm font-medium mb-1 text-black">
+        Your Title
+      </label>
+      <input
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+        placeholder="Enter your title"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
+      <label className="block text-sm font-medium mb-1 text-black">
+        Manager&apos;s Name
+      </label>
+      <input
+        name="managerName"
+        value={formData.managerName}
+        onChange={handleChange}
+        placeholder="Enter your manager's name"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-            <label className="block text-sm font-medium mb-1 text-black">Your Title</label>
-            <input
-                name="title"
-                value={formData.title}
-                onChange={handleChange}
-                placeholder="Enter your title"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
-            <label className="block text-sm font-medium mb-1 text-black">Manager&apos;s Name</label>
-            <input
-                name="managerName"
-                value={formData.managerName}
-                onChange={handleChange}
-                placeholder="Enter your manager's name"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
+      <label className="block text-sm font-medium mb-1 text-black">
+        Company
+      </label>
+      <input
+        name="company"
+        value={formData.company}
+        onChange={handleChange}
+        placeholder="Enter your company name"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-            <label className="block text-sm font-medium mb-1 text-black">Company</label>
-            <input
-                name="company"
-                value={formData.company}
-                onChange={handleChange}
-                placeholder="Enter your company name"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
+      <label className="text-sm font-medium mb-1 text-black ">
+        Specific Project
+      </label>
+      <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
+      <input
+        name="specificProject"
+        value={formData.specificProject}
+        onChange={handleChange}
+        placeholder="Enter your specific project"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-            <label className="text-sm font-medium mb-1 text-black ">Specific Project</label>
-            <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
-            <input
-                name="specificProject"
-                value={formData.specificProject}
-                onChange={handleChange}
-                placeholder="Enter your specific project"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
+      <label className=" text-sm font-medium mb-1 text-black">
+        Specific Team/Goal
+      </label>
+      <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
+      <input
+        name="specificTeam"
+        value={formData.specificTeam}
+        onChange={handleChange}
+        placeholder="Enter your specific team/goal"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-            <label className=" text-sm font-medium mb-1 text-black">Specific Team/Goal</label>
-            <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
-            <input
-                name="specificTeam"
-                value={formData.specificTeam}
-                onChange={handleChange}
-                placeholder="Enter your specific team/goal"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
+      <label className=" text-sm font-medium mb-1 text-black">
+        Travel Cost
+      </label>
+      <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
+      <input
+        name="travelCost"
+        value={formData.travelCost}
+        type="number"
+        onChange={handleChange}
+        placeholder="Enter your travel cost"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
+      <label className="text-sm font-medium mb-1 text-black">Goal</label>
+      <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
+      <input
+        name="goal"
+        value={formData.goal}
+        onChange={handleChange}
+        placeholder="specific goal, e.g., modernize IT infrastructure, enhance cloud security, improve operational efficiency"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
 
-            <label className=" text-sm font-medium mb-1 text-black">Travel Cost</label>
-            <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
-            <input
-                name="travelCost"
-                value={formData.travelCost}
-                type="number"
-                onChange={handleChange}
-                placeholder="Enter your travel cost"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
-
-            <label className="text-sm font-medium mb-1 text-black">Agenda</label>
-            <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
-            <input
-                name="agenda"
-                value={formData.agenda}
-                onChange={handleChange}
-                placeholder="Enter the conference agenda"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
-
-            <label className="block text-sm font-medium mb-1 text-black">Goal</label>
-            <input
-                name="goal"
-                value={formData.goal}
-                onChange={handleChange}
-                placeholder="specific goal, e.g., modernize IT infrastructure, enhance cloud security, improve operational efficiency"
-                className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
-            />
-
-
-
-        </form >
-    )
+      <label className="text-sm font-medium mb-1 text-black">Agenda</label>
+      <span className="text-xs text-gray-500 mb-2 pl-2">Optional</span>
+      <input
+        name="agenda"
+        value={formData.agenda}
+        onChange={handleChange}
+        placeholder="Enter the conference agenda"
+        className="w-full rounded border text-card-foreground shadow-sm bg-white border-gray-300 p-2 text-black mb-4"
+      />
+    </form>
+  );
 }
