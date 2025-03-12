3d68ce57d5dc2ab0a2e56145741df33858da7140
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-12 15:06:20 -0700

    responsive styling updated, and figma mock up designs applied

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -3,16 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { EVENT_CONFIG } from "@/lib/constants";
 import Script from "next/script";
-import StatBanner from "./stat-banner";
 import Image from "next/image";
-import { heroProviders } from "@/lib/cloud-hero-providers";
 import { useRef, useEffect } from "react";
 import Countdown from 'react-countdown';
 
 export const HeroSection = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  const Completionist = () => <span>Cloud Summit is taking off! ☁️</span>;
+  const CompletionMessage = () => (<h3 className="bg-[#313539] rounded-md flex text-4xl md:text-6xl">The Summit Has Begun! Join us in the clouds! ☁️</h3>);
   const targetDate = new Date('2025-05-27T12:00:00');
 
   useEffect(() => {
@@ -34,39 +32,71 @@ export const HeroSection = () => {
   };
 
   return (
-    <div className="relative min-h-[900px] flex flex-col items-center justify-center overflow-hidden w-full bg-hero-pattern bg-no-repeat bg-center px-4 py-16 text-white">
+    <div className="relative min-h-[900px] flex flex-col items-center justify-center gap-16  w-full bg-hero-pattern bg-no-repeat bg-center px-4 py-16 text-white">
       <Image
         src="/main-banner/banner.svg"
         alt="Hero Logo"
         width={0}
         height={0}
         sizes="100vw"
-        className="w-[90%]  h-auto"
+        className="w-[90%]  h-auto xl:w-[70%] 2xl:w-[60%] 3xl:w-[50%]"
       />
 
       <Countdown
         date={targetDate}
-        renderer={({ days, hours, minutes, seconds }) => (
-          <div>
-            {days} days {hours} hours {minutes} minutes {seconds} seconds
-          </div>
-        )}
-      />,
-      <div className="relative mx-auto flex flex-col flex-wrap justify-center gap-6">
+        renderer={({ days, hours, minutes, seconds, completed }) => {
+          if (completed) {
+            return <CompletionMessage />;
+          }
+          return (
+            <article className="bg-[#313539] rounded-md flex">
+              <div className="flex flex-col items-center justify-center gap-1 md:gap-3 m-4 md:mx-6 pr-4 border-r border-[#4d4d4d]">
+                <h3 className="text-6xl md:text-8xl text-center text-white">
+                  {days}
+                </h3>
+                <p className="text-primary-400">Days</p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-1 md:gap-3 my-4 mr-4 md:mr-6 pr-4 md:pr-6 border-r border-[#4d4d4d]">
+                <h3 className="text-6xl md:text-8xl text-center text-white">
+                  {hours}
+                </h3>
+                <p className="text-primary-400">Hours</p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-1 md:gap-2 my-4 mr-4 md:mr-6 pr-4 md:pr-6 border-r border-[#4d4d4d]">
+                <h3 className="text-6xl md:text-8xl text-center text-white">
+                  {minutes}
+                </h3>
+                <p className="text-primary-400">Minutes</p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-1 md:gap-2 my-4 mr-4 md:mr-6">
+                <h3 className="text-6xl md:text-8xl text-center text-white">
+                  {seconds}
+                </h3>
+                <p className="text-primary-400">Seconds</p>
+              </div>
+
+            </article>
+          )
+        }}
+      />
+      <div className="relative mx-auto flex flex-col flex-wrap justify-center gap-10">
+        <p className="mx-auto max-w-3xl text-center text-xl text-gray-300 md:text-3xl font-bold uppercase">
+          {EVENT_CONFIG.date} at {EVENT_CONFIG.location.city} {EVENT_CONFIG.venue}
+        </p>
         {/* Description */}
-        <p className="mx-auto mb-8 max-w-3xl text-center text-lg text-gray-300 md:text-xl">
+        <p className="mx-auto max-w-3xl text-center text-md text-gray-300 md:text-xl">
           {EVENT_CONFIG.description}
         </p>
 
         {/* CTA Buttons */}
-        <div className="mb-2 md:mb-16 flex flex-col items-center justify-center gap-4 md:flex-row">
+        <div className="mb-2 md:mb-16 flex flex-row items-center justify-evenly md:justify-center gap-4 ">
           <a
             href={EVENT_CONFIG.links.tickets}
-            className="min-w-[200px] h-11 flex justify-center items-center rounded-md bg-gradient-to-r from-green-900 to-green-500 font-bold"
+            className="min-w-[150px] h-11 flex justify-center items-center rounded-md bg-secondary-600 hover:bg-secondary-800"
             data-luma-action="checkout"
             data-luma-event-id="evt-cItbLfgBkf8na4n"
           >
-            Get Tickets
+            Get tickets
           </a>
 
           <Script
@@ -78,39 +108,15 @@ export const HeroSection = () => {
           <Button
             size="lg"
             variant="outline"
-            className="min-w-[200px] border-white/20 text-white hover:bg-white/10 hidden md:block"
+            className="min-w-[150px] border-white/20 text-white hover:bg-white/10  md:block"
             onClick={openVolunteersLink}
           >
-            GET INVOLVED
+            Get involved
           </Button>
         </div>
-
-        {/* Stats */}
-        <StatBanner />
       </div>
 
-      {/* Cloud Providers */}
-      <div className="grid grid-cols-4 gap-4 mt-4 md:mt-16 md:grid-cols-4 md:flex md:flex-row md:overflow-hidden md:whitespace-nowrap">
-        {heroProviders.map((provider) => (
-          <a
-            href={provider.link}
-            key={provider.name}
-            target="_blank"
-            className="relative"
-          >
-            <div className="relative w-14 h-14">
-              <Image
-                src={provider.icon}
-                alt={provider.name}
-                width={56}
-                height={56}
-                className="object-contain"
-                unoptimized
-              />
-            </div>
-          </a>
-        ))}
-      </div>
+
     </div>
   );
 };
