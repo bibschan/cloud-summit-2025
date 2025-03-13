7735e6e3fc33c061bacdfc5ab5b8f8d9944f4984
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-12 19:10:14 -0700

    speakers file added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import { MediaLounge } from "@/components/media-lounge-section";
 import Footer from "@/components/footer";
 import CharitySection from "@/components/charity-section";
 import { CommunitiesList } from "@/components/communities";
+import { Speaker } from "lucide-react";
+import SpeakersSection from "@/components/speakers-section";
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
@@ -18,8 +20,9 @@ export default function Home() {
         <HeroSection />
         <HighlightsSection />
         <SponsorsSection />
+        <SpeakersSection />
         <MediaLounge />
-        {/* <CommunitySection /> */}
+
         <VenueSection />
         <CommunitiesList />
         <CharitySection />
--- src/components/speakers-section.tsx
+++ src/components/speakers-section.tsx
@@ -1,12 +1,15 @@
 'use client'
 import React from 'react'
 
-function Speakers() {
+function SpeakersSection() {
   return (
-    <section>
-        <h2>Our Speakers</h2>
+    <section className="py-20 bg-gradient-to-b from-black to-gray-900" id="venue">
+      <div className="container mx-auto px-4 xl:max-w-[1100px]">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+          Our Speakers</h2>
+          </div>
     </section>
   )
 }
 
-export default Speakers
+export default SpeakersSection
