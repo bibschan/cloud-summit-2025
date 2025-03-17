ebb90fbd4d5a6b1c3753acd1665d611c9400c720
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-17 12:11:52 -0700

    favicon updated

--- src/components/faq-section.tsx
+++ src/components/faq-section.tsx
@@ -51,7 +51,7 @@ export const FAQSection = () => {
   return (
     <section className="py-20 bg-primary-900" id="faq">
       <div className="container mx-auto px-4 xl:max-w-[1100px]">
-        <h2 className="text-6xl md:text-8xl  font-bold mb-10 text-center bg-clip-text text-white">
+        <h2 className="text-6xl md:text-8xl  mb-10 text-center bg-clip-text text-white">
           Some <span className="text-secondary-600">{EVENT_CONFIG.sections.faq.title}</span>
         </h2>
         <Accordion type="single" collapsible className="w-full mx-auto">
