e1037be4f43e71455c57d253197310fa673f4c73
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-24 15:43:49 -0700

    team page added

---
+++ src/app/team/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "@/components/nav";
+import Footer from "@/components/footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen text-white ">
+      <Nav />
+      <main>
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
