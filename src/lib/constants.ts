116a0c8773dd02572c71bc23d3601d73e8f98521
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-03-24 13:44:08 -0700

    faq updated to match mock up

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -167,7 +167,7 @@ export const EVENT_CONFIG = {
         {
           question: "Is the event accessible?",
           answer:
-            "Yes, there is both accessible ramps, elevators, and main stage seating. For more details check out the Vancouver Orpheum Theatre site for details https://vancouvercivictheatres.com/accessibility/#accessibility.",
+            "Yes, there is both accessible ramps, elevators, and main stage seating. For more details check out the Vancouver Orpheum Theatre site for details https://vancouvercivictheatres.com/accessibility/.",
         },
         {
           question: "Will I be able to watch the event online?",
