2fc9dfc411cb6b4d9f6d4dbc77f78b5b8e71c595
Sophia Chan <sophia.gy.chan@gmail.com>
2025-03-27 07:31:50 -0700

    site wide message section added (#37)
    * site wide message section added
    
    * text-center
    
    * set to false no message now
    
    * remove vote word from nav, still needs to be updated to vote page
    
    * added env variables instead, still uses state for the close button
    
    * moved state to parent so that the nav can move up when the message is closed
    
    * session storage added
    
    * added margin top to hero image

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,8 +9,11 @@ import { ArrowLeft, Menu, X } from "lucide-react";
 import { isAdmin } from "@/lib/admin";
 import Image from "next/image";
 import styles from "./nav.module.css";
+interface NavProps {
+  showMessage?: string;
+}
 
-export default function Nav() {
+export default function Nav({ showMessage = 'hidden' }: NavProps) {
   const [isAtTop, setIsAtTop] = useState(true);
   const [hasVoted, setHasVoted] = useState(true);
   const { data: session } = useSession();
@@ -68,7 +71,7 @@ export default function Nav() {
 
   // Render different nav content based on route
   const renderNavContent = () => {
-    if (isSignInPage || isVotePage || isAdminPage || isNominatePage) {
+    if (isSignInPage || isAdminPage || isNominatePage) {
       return (
         <div className="container mx-auto px-6 flex justify-between items-center">
           <Link
@@ -244,7 +247,7 @@ export default function Nav() {
             </>
           ) : (
             <Link href="/auth/signin" className="hover:text-sky-400">
-              Vote
+
             </Link>
           )}
         </div>
@@ -275,7 +278,7 @@ export default function Nav() {
       ${
         isSignInPage || isVotePage || isAdminPage || isNominatePage
           ? "absolute py-4"
-          : `fixed top-0 py-5 transition duration-300 ease-in-out ${
+          : `fixed ${showMessage === 'visible' ? 'top-16' : 'top-0'} py-5 transition duration-300 ease-in-out ${
               !isAtTop ? "bg-black/50 backdrop-blur-md shadow-xl" : ""
             }`
       }
---
+++ src/components/site-wide-section.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { useState } from 'react';
+import { X } from 'lucide-react'
+
+interface SiteWideMessageProps {
+    isVisible: string;
+    handleClose: () => void;
+  }
+
+export const SiteWideMessage: React.FC<SiteWideMessageProps> = ({ isVisible, handleClose }) => {
+    const message = process.env.NEXT_PUBLIC_BANNER_MESSAGE || 'Default message';
+
+    if (isVisible === 'hidden' || isVisible === 'initial') return null;
+
+    return (
+        <div
+            className={`fixed top-0 left-0 w-full z-50 p-4 } flex items-center justify-between shadow-md transition-all duration-300 ease-in-out animate-slide-down bg-brink-pink`}
+        >
+            <div className="text-center flex-grow mr-4">
+                {message}
+            </div>
+            <button
+                onClick={handleClose}
+                className="rounded-full p-1 transition-colors duration-200 "
+                aria-label="Close message"
+            >
+                <X size={24} />
+            </button>
+        </div>
+    );
+};
