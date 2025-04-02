3c3b464fcd3eed1065d25f34a866c6775c6abaf6
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-04-01 17:48:25 -0700

    hightlight section animation added

--- src/components/highlights-section.tsx
+++ src/components/highlights-section.tsx
@@ -1,14 +1,29 @@
-import { EVENT_CONFIG } from "@/lib/constants";
-import Image from "next/image";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+'use client'
+import { useEffect, useState, useRef } from 'react';
+import Image from 'next/image';
+import { motion, useTransform, useScroll } from 'framer-motion';
 
 export const HighlightsSection = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const section = document.getElementById('highlights');
+      if (!section) return;
+      const rect = section.getBoundingClientRect();
+      const isInView = rect.top < window.innerHeight - 100 && rect.bottom >= 0;
+      setIsVisible(isInView);
+    };
+
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
     <section className="py-20  bg-primary-900" id="highlights">
       <div
@@ -24,14 +39,22 @@ export const HighlightsSection = () => {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="rounded-2xl bg-lemon-lime p-4 col-span-3 row-span-2 text-black flex flex-col justify-center text-center">
+        <motion.div
+          className="rounded-2xl bg-lemon-lime p-4 col-span-3 row-span-2 text-black flex flex-col justify-center text-center"
+          initial={{ opacity: 0, y: -100 }}
+          animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: -100 }}
+          transition={{
+            duration: isVisible ? 0.8 : 0.5,
+            ease: isVisible ? "easeOut" : "easeIn"
+          }}
+        >
           <h3 className={`font-highlight text-xl md:text-8xl font-semibold`}>
             $10,000+
           </h3>
           <p className="font-semibold text-sm md:text-lg">
             Charity Donation Goal
           </p>
-        </div>
+        </motion.div>
         <div className="hidden md:block rounded-2xl col-span-2 row-span-2">
           <Image
             src="/past-events/AWSDay-7.svg"
@@ -51,22 +74,37 @@ export const HighlightsSection = () => {
             className="m-auto"
           />
         </div>
-        <div className="rounded-2xl bg-secondary-600 p-4 col-span-2 row-span-2 text-white flex flex-col justify-center text-center">
+        <motion.div
+          className="rounded-2xl bg-secondary-600 p-4 col-span-2 row-span-2 text-white flex flex-col justify-center text-center"
+          initial={{ opacity: 0, x: 100 }}
+          animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }}
+          transition={{
+            duration: isVisible ? 0.8 : 0.5,
+            ease: isVisible ? "easeOut" : "easeIn"
+          }}
+        >
           <h3 className={`font-highlight text-xl md:text-8xl font-semibold`}>
             780+
           </h3>
           <p className="font-semibold text-sm md:text-lg">
             In-Person Attendees
           </p>
-        </div>
-        <div className="rounded-2xl bg-lilac p-4 col-span-2 row-span-2 flex flex-col justify-center text-center">
+        </motion.div>
+        <motion.div
+          className="rounded-2xl bg-lilac p-4 col-span-2 row-span-2 flex flex-col justify-center text-center"
+          initial={{ opacity: 0, x: -100 }}
+          animate={isVisible ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+          transition={{
+            duration: isVisible ? 1 : 0.5,
+            ease: isVisible ? "easeOut" : "easeIn"
+          }}>
           <h3 className={`font-highlight text-xl md:text-8xl font-semibold`}>
             21+
           </h3>
           <p className="font-semibold text-sm md:text-lg">
             Presentations & Workshops
           </p>
-        </div>
+        </motion.div>
         <div className="hidden md:block rounded-2xl col-span-2 row-span-2">
           <Image
             src="/past-events/AWSDay-9.svg"
@@ -86,14 +124,21 @@ export const HighlightsSection = () => {
             className="w-full h-full rounded-2xl object-cover"
           />
         </div>
-        <div className="rounded-2xl bg-brink-pink p-4 col-span-2 row-span-1 flex flex-col justify-center text-center text-black">
+        <motion.div
+          className="rounded-2xl bg-brink-pink p-4 col-span-2 row-span-1 flex flex-col justify-center text-center text-black"
+          initial={{ opacity: 0, y: 100 }}
+          animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+          transition={{
+            duration: isVisible ? 1.5 : 0.5,
+            ease: isVisible ? "easeOut" : "easeIn"
+          }}>
           <h3 className={`font-highlight text-xl md:text-8xl font-semibold`}>
             12+
           </h3>
           <p className="font-semibold text-sm md:text-lg">
             Sponsors & Communities
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
