c58213e53ee061d222284af54d5782e7357f7dab
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2025-04-04 09:51:14 -0700

    venue page updated responsive

--- src/app/venue/page.tsx
+++ src/app/venue/page.tsx
@@ -12,12 +12,13 @@ import { Button } from "@/components/ui/button";
 export default function VenuePage() {
 
     const images = [
-        "/venue/CloudSummit_2025_Floorplans_MainFloor.svg",
-        "/venue/CloudSummit_2025_Floorplans_2ndFloor.svg",
-        "/venue/CloudSummit_2025_Floorplans_3rdFloor.svg",
+        { src: "/venue/CloudSummit_2025_Floorplans_MainFloor.svg", title: "Main Floor" },
+        { src: "/venue/CloudSummit_2025_Floorplans_2ndFloor.svg", title: "2nd Floor" },
+        { src: "/venue/CloudSummit_2025_Floorplans_3rdFloor.svg", title: "3rd Floor" },
     ];
 
-    const [selectedImage, setSelectedImage] = useState(images[0]);
+
+    const [selectedImage, setSelectedImage] = useState(images[0].src);
 
     return (
         <div className="min-h-screen text-white bg-primary-900 ">
@@ -30,21 +31,26 @@ export default function VenuePage() {
                     </h1>
                     <VenueSection />
 
-                    <div className="w-full flex flex-col item-center justify-center gap-4  mb-8 ">
+                    <div className="w-full flex flex-col item-center justify-center gap-4  mb-8 " aria-label="Venue Maps contains one large image of the selected floor and a list of the floor from Main, 2nd floor, and 3rd floor.">
                         <div className="relative w-full max-w-4xl h-auto aspect-video mx-auto ">
                             <Image src={selectedImage} alt="Selected" fill className="object-cover rounded-lg" />
                         </div>
-                        <div className="flex gap-4 justify-center">
-
+                        <ul className="flex gap-4 justify-center md:flex-row max-w-4xl w-full mx-auto overflow-x-scroll">
                             {images.map((img, index) => (
-                                <div key={index} className="relative w-52 md:w-64 h-auto cursor-pointer aspect-video" onClick={() => setSelectedImage(img)}>
-                                    <Image src={img} alt={`Thumbnail ${index + 1}`} fill
-                                        className={`object-cover rounded-lg border-[3px] ${selectedImage === img ? 'border-primary-200' : 'border-transparent'
-                                            } hover:border-primary-200`}
-                                    />
-                                </div>
+                                <li key={index} className=" w-4/6 md:w-64 h-auto cursor-pointer aspect-video flex flex-col items-center"
+                                    onClick={() => setSelectedImage(img.src)}
+                                >
+                                    <div className="relative w-4/6 md:w-64 h-auto cursor-pointer aspect-video flex flex-col items-center">
+                                        <Image src={img.src} alt={`Thumbnail ${index + 1}`} fill
+                                            className={`object-cover rounded-lg border-[3px] ${selectedImage === img.src ? 'border-primary-200' : 'border-transparent'
+                                                } hover:border-primary-200`}
+                                        />
+                                    </div>
+
+                                    <p className="text-center text-sm mt-2">{img.title}</p>
+                                </li>
                             ))}
-                        </div>
+                        </ul>
                         <Link href="https://vancouvercivictheatres.com/accessibility/#accessibility" target="_blank" className="mx-auto my-8">
                             <Button
                                 size="lg"
