fb5bac41ec09971aebeefab573b99aafb5b7e455
bibianasouza <souzabibiana@hotmail.com>
2025-04-19 07:09:29 -0700

    padding around the button

--- src/components/toronto-event-banner.tsx
+++ src/components/toronto-event-banner.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function TorontoEventBanner(): React.JSX.Element {
   return (
-    <div className="left-0 z-40 flex items-center justify-between shadow-md transition-all duration-300 ease-in-out animate-slide-down bg-brink-pink w-full bg-gradient-to-r from-cyan-600 to-blue-700 py-2 px-4 text-white">
+    <div className="left-0 z-40 flex items-center justify-between shadow-md transition-all duration-300 ease-in-out animate-slide-down bg-brink-pink w-full bg-gradient-to-r from-cyan-600 to-blue-700 text-white p-4">
       <div className="container mx-auto flex flex-col items-center justify-between gap-2 sm:flex-row">
         <div className="text-center sm:text-left">
           <p className="font-medium">
