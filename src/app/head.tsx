7cad45ff134fbfce32e7f06770b9738af037f588
bibianasouza <souzabibiana@hotmail.com>
2025-05-16 14:22:41 -0700

    fixed gtag again

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,16 +3,18 @@ import Script from "next/script";
 export default function Head() {
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google Ads Conversion Tracking */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-957966437"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);} 
-        gtag('js', new Date());
-        gtag('config', 'AW-957966437');`}
+      <Script id="google-ads" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-957966437');
+        `}
       </Script>
     </>
   );
