528afd3d4a04755b04ba0c514fcfaaf7147d56c5
bibianasouza <souzabibiana@hotmail.com>
2025-05-16 14:16:44 -0700

    gtag fix

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,20 +1,18 @@
-import Script from 'next/script';
+import Script from "next/script";
 
 export default function Head() {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
   return (
     <>
+      {/* Google Analytics */}
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="beforeInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-957966437"
+        strategy="afterInteractive"
       />
       <Script id="gtag-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}');
-        `}
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+        gtag('config', 'AW-957966437');`}
       </Script>
     </>
   );
